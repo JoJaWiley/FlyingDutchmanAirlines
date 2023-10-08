87e6a8a182e4b376e812f91928f6ea4103a8b4e0
JoJaWiley <joshua.wiley@summitoic.edu>
2023-10-08 18:45:52 -0500

    First commit - configuration, EFC data layer, tests, and the start of the customer repo class

---
+++ FlyingDutchmanAirlines/RepositoryLayer/CustomerRepository.cs
@@ -0,0 +1,30 @@
+﻿using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.DatabaseLayer.Models;
+
+namespace FlyingDutchmanAirlines.RepositoryLayer;
+
+public class CustomerRepository
+{
+    public async Task<bool> CreateCustomer(string name)
+    {
+        if (IsInvalidCustomerName(name))
+        {
+            return false;
+        }
+
+        Customer newCustomer = new Customer(name);
+        using (FlyingDutchmanAirlinesContext context = new FlyingDutchmanAirlinesContext())
+        {
+            context.Customers.Add(newCustomer);
+            await context.SaveChangesAsync();
+        }
+
+        return true;
+    }
+
+    private bool IsInvalidCustomerName(string name)
+    {
+        char[] forbiddenCharacters = { '!', '@', '#', '$', '%', '&', '*' };
+        return string.IsNullOrEmpty(name) || name.Any(x => forbiddenCharacters.Contains(x));
+    }
+}
---
+++ FlyingDutchmanAirlines/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlyingDutchmanAirlines;
+
+public class Startup
+{
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseRouting();
+        app.UseEndpoints(endpoints => endpoints.MapControllers());
+    }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddControllers();
+    }
+}
