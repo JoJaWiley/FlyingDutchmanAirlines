48c05d4951ba1c46e5ca5b91403322e9f6f6d638
JoJaWiley <joshua.wiley@summitoic.edu>
2023-10-08 20:23:25 -0500

    DI for dbcontext in CustomerRepository, try catch in CreateCustomer, tests use DI for dbcontext and test the try catch

--- FlyingDutchmanAirlines_Tests/RepositoryLayer/CustomerRepositoryTests.cs
+++ FlyingDutchmanAirlines_Tests/RepositoryLayer/CustomerRepositoryTests.cs
@@ -1,14 +1,27 @@
 using FlyingDutchmanAirlines.RepositoryLayer;
+using Microsoft.EntityFrameworkCore;
+using FlyingDutchmanAirlines.DatabaseLayer;
 
 namespace FlyingDutchmanAirlines_Tests.RepositoryLayer;
 
 [TestClass]
 public class CustomerRepositoryTests
 {
+    private FlyingDutchmanAirlinesContext _context;
+
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        DbContextOptions<FlyingDutchmanAirlinesContext> dbContextOptions = new
+                DbContextOptionsBuilder<FlyingDutchmanAirlinesContext>()
+            .UseInMemoryDatabase("FlyingDutchman").Options;
+        _context = new FlyingDutchmanAirlinesContext(dbContextOptions);
+    }
+    
     [TestMethod]
     public async Task CreateCustomer_Success()
     {
-        CustomerRepository repository = new CustomerRepository();
+        CustomerRepository repository = new CustomerRepository(_context);
         Assert.IsNotNull(repository);
 
         bool result = await repository.CreateCustomer("Donald Knuth");
@@ -18,7 +31,7 @@ public class CustomerRepositoryTests
     [TestMethod]
     public async Task CreateCustomer_Failure_NameIsNull()
     {
-        CustomerRepository repository = new CustomerRepository();
+        CustomerRepository repository = new CustomerRepository(_context);
         Assert.IsNotNull(repository);
 
         bool result = await repository.CreateCustomer(null);
@@ -28,7 +41,7 @@ public class CustomerRepositoryTests
     [TestMethod]
     public async Task CreateCustomer_Failure_NameIsEmptyString()
     {
-        CustomerRepository repository = new CustomerRepository();
+        CustomerRepository repository = new CustomerRepository(_context);
         Assert.IsNotNull(repository);
 
         bool result = await repository.CreateCustomer("");
@@ -45,10 +58,20 @@ public class CustomerRepositoryTests
     [DataRow('*')]
     public async Task CreateCustomer_Failure_NameContainsInvalidCharacters(char invalidCharacter)
     {
-        CustomerRepository repository = new CustomerRepository();
+        CustomerRepository repository = new CustomerRepository(_context);
         Assert.IsNotNull(repository);
 
         bool result = await repository.CreateCustomer("Donald Knuth" + invalidCharacter);
+        Assert.IsFalse(result);
+    }
+
+    [TestMethod]
+    public async Task CreateCustomer_Failure_DatabaseAccessError()
+    {
+        CustomerRepository repository = new CustomerRepository(null);
+        Assert.IsNotNull(repository);
+
+        bool result = await repository.CreateCustomer("Donald Knuth");
         Assert.IsFalse(result);
     }
     
