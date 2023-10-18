5b8093ebfa0e396534b42515ab7612417fd980f3
JoJaWiley <joshua.wiley@summitoic.edu>
2023-10-17 20:17:51 -0500

    booking repo class created

---
+++ FlyingDutchmanAirlines/RepositoryLayer/BookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlyingDutchmanAirlines.DatabaseLayer;
+
+namespace FlyingDutchmanAirlines.RepositoryLayer;
+
+public class BookingRepository
+{
+    private readonly FlyingDutchmanAirlinesContext _context;
+
+    public BookingRepository(FlyingDutchmanAirlinesContext _context)
+    {
+        this._context = _context;
+    }
+}
