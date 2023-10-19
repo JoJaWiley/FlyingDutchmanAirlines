2c067616ffc06876d8f8a8642b33d33dfd976aae
JoJaWiley <joshua.wiley@summitoic.edu>
2023-10-18 20:46:14 -0500

    CreateBooking input validation

--- FlyingDutchmanAirlines/RepositoryLayer/BookingRepository.cs
+++ FlyingDutchmanAirlines/RepositoryLayer/BookingRepository.cs
@@ -10,4 +10,14 @@ public class BookingRepository
     {
         this._context = _context;
     }
+
+    public async Task CreateBooking(int customerID, int flightNumber)
+    {
+        if (customerID < 0 || flightNumber < 0)
+        {
+            Console.WriteLine($"Argument exception in CreateBooking! CustomerID" +
+                              $" = {customerID}, flightNumber = {flightNumber}");
+            throw new ArgumentException("Invalid Arguments Provided");
+        }
+    }
 }
