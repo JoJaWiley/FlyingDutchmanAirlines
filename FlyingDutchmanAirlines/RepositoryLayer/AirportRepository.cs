b0a4b293fb5403b40fee35fa568bc0e5ace9a9ca
JoJaWiley <joshua.wiley@summitoic.edu>
2023-10-21 20:24:38 -0500

    AirportRepository and AirportRepositoryTests skeleta added

---
+++ FlyingDutchmanAirlines/RepositoryLayer/AirportRepository.cs
@@ -0,0 +1,13 @@
+﻿using FlyingDutchmanAirlines.DatabaseLayer;
+
+namespace FlyingDutchmanAirlines.RepositoryLayer;
+
+public class AirportRepository
+{
+    private readonly FlyingDutchmanAirlinesContext _context;
+
+    public AirportRepository(FlyingDutchmanAirlinesContext _context)
+    {
+        this._context = _context;
+    }
+}
---
+++ FlyingDutchmanAirlines_Tests/RepositoryLayer/AirportRepositoryTests.cs
@@ -0,0 +1,24 @@
+﻿using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.RepositoryLayer;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlyingDutchmanAirlines_Tests.RepositoryLayer;
+
+[TestClass]
+public class AirportRepositoryTests
+{
+    private FlyingDutchmanAirlinesContext _context;
+    private AirportRepository _repository;
+    
+    [TestInitialize]
+    public void TestInitialize()
+    {
+        DbContextOptions<FlyingDutchmanAirlinesContext> dbContextOptions =
+            new DbContextOptionsBuilder<FlyingDutchmanAirlinesContext>()
+                .UseInMemoryDatabase("FlyingDutchman").Options;
+
+        _context = new FlyingDutchmanAirlinesContext(dbContextOptions);
+        _repository = new AirportRepository(_context);
+        Assert.IsNotNull(_repository);
+    }
+}
