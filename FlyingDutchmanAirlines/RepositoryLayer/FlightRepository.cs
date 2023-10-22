d25a8304e267d6b403a9ce8c0173143cc0188f00
JoJaWiley <joshua.wiley@summitoic.edu>
2023-10-22 00:19:38 -0500

    FlightRepository and FlightRepositoryTests skeleta created.

---
+++ FlyingDutchmanAirlines/RepositoryLayer/FlightRepository.cs
@@ -0,0 +1,19 @@
+﻿using FlyingDutchmanAirlines.DatabaseLayer;
+using FlyingDutchmanAirlines.DatabaseLayer.Models;
+
+namespace FlyingDutchmanAirlines.RepositoryLayer;
+
+public class FlightRepository
+{
+    private readonly FlyingDutchmanAirlinesContext _context;
+
+    public FlightRepository(FlyingDutchmanAirlinesContext _context)
+    {
+        this._context = _context;
+    }
+
+    public async Task<Flight> GetFlightByFlightNumber(int flightNumber)
+    {
+        return new Flight();
+    }
+}
