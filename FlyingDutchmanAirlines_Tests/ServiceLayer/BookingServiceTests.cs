ae2407a0aa5b688de30e023993708fc00c6ee657
JoJaWiley <joshua.wiley@summitoic.edu>
2023-10-22 18:13:50 -0500

    BookingService input validation and associated failure tests.

--- FlyingDutchmanAirlines_Tests/ServiceLayer/BookingServiceTests.cs
+++ FlyingDutchmanAirlines_Tests/ServiceLayer/BookingServiceTests.cs
@@ -36,4 +36,23 @@ public class BookingServiceTests
         Assert.IsTrue(result);
         Assert.IsNull(exception);
     }
+
+    [TestMethod]
+    [DataRow("", 0)]
+    [DataRow(null, -1)]
+    [DataRow("Galileo Galilei", -1)]
+    public async Task CreateBooking_Failure_InvalidInputArguments(string name, int flightNumber)
+    {
+        Mock<BookingRepository> mockBookingRepository = new Mock<BookingRepository>();
+        Mock<CustomerRepository> mockCustomerRepository = new Mock<CustomerRepository>();
+
+        BookingService service =
+            new BookingService(mockBookingRepository.Object, mockCustomerRepository.Object);
+
+        (bool result, Exception? exception) =
+            await service.CreateBooking(name, flightNumber);
+        
+        Assert.IsFalse(result);
+        Assert.IsNotNull(exception);
+    }
 }
