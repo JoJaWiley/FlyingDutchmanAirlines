b400e252c39b5ee9b19ab99dbe26c1faa2f8e623
JoJaWiley <joshua.wiley@summitoic.edu>
2023-11-10 21:55:34 -0600

    FlightController, dependency injection, and success case test.

---
+++ FlyingDutchmanAirlines/ControllerLayer/FlightController.cs
@@ -0,0 +1,40 @@
+﻿using System.Net;
+using FlyingDutchmanAirlines.Exceptions;
+using FlyingDutchmanAirlines.ServiceLayer;
+using FlyingDutchmanAirlines.Views;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlyingDutchmanAirlines.ControllerLayer;
+
+public class FlightController : Controller
+{
+    private readonly FlightService _service;
+
+    public FlightController(FlightService service)
+    {
+        _service = service;
+    }
+    public async Task<IActionResult> GetFlights()
+    {
+        try
+        {
+            Queue<FlightView> flights = new Queue<FlightView>();
+            await foreach (FlightView flight in _service.GetFlights())
+            {
+                flights.Enqueue(flight);
+            }
+
+            return StatusCode((int)HttpStatusCode.OK, flights);
+        }
+        catch (FlightNotFoundException)
+        {
+            return StatusCode((int)HttpStatusCode.NotFound,
+                "No flights were found in the database");
+        }
+        catch (Exception)
+        {
+            return StatusCode((int)HttpStatusCode.InternalServerError,
+                "An error occurred");
+        }
+    }
+}
