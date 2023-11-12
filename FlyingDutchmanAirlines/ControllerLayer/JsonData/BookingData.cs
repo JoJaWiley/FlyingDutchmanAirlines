1f2d643fb9667eb4de147d967d71f2a6087d7772
JoJaWiley <joshua.wiley@summitoic.edu>
2023-11-11 21:04:49 -0600

    BookingController skeleton and BookingData plus BD unit test

---
+++ FlyingDutchmanAirlines/ControllerLayer/JsonData/BookingData.cs
@@ -0,0 +1,25 @@
+﻿namespace FlyingDutchmanAirlines.ControllerLayer.JsonData;
+
+public class BookingData
+{
+    private string _firstName;
+
+    public string FirstName
+    {
+        get => _firstName; 
+        set => _firstName = ValidateName(value, nameof(FirstName));
+    }
+
+    private string _lastName;
+
+    public string LastName
+    {
+        get => _lastName; 
+        set => _lastName = ValidateName(value, nameof(LastName));
+    }
+
+    private string ValidateName(string name, string propertyName) =>
+        string.IsNullOrEmpty(name)
+            ? throw new InvalidOperationException("could not set " + propertyName)
+            : name;
+}
